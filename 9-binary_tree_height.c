70d7fd3fd79684bc63ff67c41091e43d83de4878
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-10 09:02:34 +0000

    Task 9, added condition if tree is only made of a single node

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,19 +10,24 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_h;
 	size_t right_h;
+
 	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
 	{
 		return (0);
 	}
 	left_h = binary_tree_height(tree->left);
 	right_h = binary_tree_height(tree->right);
+
 	if (left_h > right_h)
-        {
-	 return (1 + left_h);
+	{
+		return (1 + left_h);
 	}
 	else
 	{
-		
 		return (1 + right_h);
 	}
 }
