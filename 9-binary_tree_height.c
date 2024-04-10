68e5158b8cfc6b3941553f8661695819f7b5662b
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-10 08:58:17 +0000

    Task 9, declared variables at begininng

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,12 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-
+	size_t left_h;
+	size_t right_h;
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	size_t left_h;
-	size_t right_h;
 	left_h = binary_tree_height(tree->left);
 	right_h = binary_tree_height(tree->right);
 	if (left_h > right_h)
