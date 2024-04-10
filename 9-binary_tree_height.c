57954c45345b123865843a040163b4fefa183fde
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-10 08:56:39 +0000

    Task 9, output not exact yet

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,25 +8,22 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	size_t left_height;
-	size_t right_height;
-	size_t max_height;
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);	
-	if (left_height > right_height)
-	{
-		max_height = left_height + 1;
-		return (max_height);
+	size_t left_h;
+	size_t right_h;
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+	if (left_h > right_h)
+        {
+	 return (1 + left_h);
 	}
 	else
 	{
-		max_height = right_height + 1;
-		return (max_height);
+		
+		return (1 + right_h);
 	}
-	
 }
-
